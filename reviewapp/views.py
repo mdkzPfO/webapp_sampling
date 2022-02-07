ec262e333a1da5a0f3a5ffd8a2dee27a1178a92d
mdkzPfO <31dpmlmdkzhbr@gmail.com>
2022-02-07 19:38:01 +0900

    first commit

---
+++ reviewapp/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import authenticate,login,logout
+from .models import ReviewModel
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+def loginview(request):
+    if request.method=='POST':
+        username_data=request.POST['username_data']
+        password_data=request.POST['password_data']
+        user=authenticate(request,username=username_data,password=password_data)
+        if user is not None:
+            login(request,user)
+            return redirect('list')
+        else:
+            return redirect('login')
+    return render(request,'login.html')
+
+def signupview(request):
+    print(request.POST.get('username_data'))
+    if request.method=='POST':
+        username_data=request.POST['username_data']
+        password_data=request.POST['password_data']
+        try:
+            User.objects.create_user(username_data,'',password_data)
+        except IntegrityError:
+            print("わっしょい")
+            return render(request,'signup.html',{'error':'このユーザーは既に登録されています'})
+    else:
+        return render(request,'signup.html',{})
+    return render(request,'signup.html',{})
+@login_required
+def listview(request):
+    object_list=ReviewModel.objects.all()
+    return render(request,'list.html',{'object_list':object_list})
+def detailview(request,pk):
+    object=ReviewModel.objects.get(pk=pk)
+    return render(request,'detail.html',{'object':object})
+
+class CreateClass(CreateView):
+    template_name='create.html'
+    model=ReviewModel
+    fields=('title','content','author','iamges','evaluation')
+    success_url=reverse_lazy('list')
+def logoutview(request):
+    logout(request)
+    return redirect('login')
+def evaluationview(request,pk):
+    post=ReviewModel.objects.get(pk=pk)
+    author_name=request.user.get_username()+str(request.user.id)
+    if author_name in post.useful_review_record:
+        return redirect('list')
+    else:
+        post.useful_review=post.useful_review+1
+        post.useful_review_record=post.useful_review_record+author_name
+        post.save()
+        return redirect('list')
